03d3a5a378c6a29fa8ecef5e1b336c8beb07b243
Hannankhan203 <ak1818375@gmail.com>
2025-08-14 19:26:15 +0500

    Firebase Configuration

---
+++ firebase.js
@@ -0,0 +1,35 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth";
+import { getFirestore, collection, addDoc, getDocs, updateDoc, deleteDoc, doc, query, where, orderBy } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDo2dPzhu3xbfYidYcGsm2zN4gYxI4nQDI",
+  authDomain: "gpa-calculator-865b3.firebaseapp.com",
+  projectId: "gpa-calculator-865b3",
+  storageBucket: "gpa-calculator-865b3.firebasestorage.app",
+  messagingSenderId: "411597084474",
+  appId: "1:411597084474:web:50b9fdcebf52ddf247a203",
+  measurementId: "G-ZZKCCRTE5V"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { 
+    auth,
+    db,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    sendEmailVerification,
+    sendPasswordResetEmail,
+    collection,
+    addDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    doc,
+    query,
+    where,
+    orderBy
+ }
