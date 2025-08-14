4b2c63fa47049ccf5cca9dcecf9216a433eeea81
Hannankhan203 <ak1818375@gmail.com>
2025-08-14 19:51:09 +0500

    Making Protected Routes using Zustand

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router-dom";
+import { useAuthStore } from "../context/store";
+
+export const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useAuthStore();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
