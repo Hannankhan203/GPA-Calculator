b06413e4ec3f82a2c109df2beba6a84ff8962083
Hannankhan203 <ak1818375@gmail.com>
2025-08-14 22:40:04 +0500

    Made a Forgot Password component

---
+++ src/components/Auth/ForgotPassword.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth, sendPasswordResetEmail } from "../../firebase";
+import { useAuthStore } from "../../context/store";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const { setError } = useAuthStore();
+  const [authError, setAuthError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const initialValues = {
+    email: "",
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Invalid email").required("Required"),
+  });
+
+  const onSubmit = async (values) => {
+    try {
+      await sendPasswordResetEmail(
+        auth,
+        values.email,
+      );
+      setSuccess("Password reset email has been sent. Check your inbox");
+      setAuthError("")
+    } catch (error) {
+      setError(error.message);
+      setAuthError(error.message);
+      setSuccess("")
+    }
+  };
+
+  return (
+    <div className="auth-container">
+      <h2 className="auth-title">Reset Password</h2>
+      {authError && <div className="error-message">{authError}</div>}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <Form className="auth-form">
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+          </div>
+          <button type="submit">Reset Password</button>
+        </Form>
+      </Formik>
+      <div className="auth-nav">
+        <Link to="/login">Back to Login</Link>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPassword;
