cb77528ac4de6ae4bd62f1a606ef2b3f145933df
Hannankhan203 <ak1818375@gmail.com>
2025-08-18 15:49:44 +0500

    Fixed custom route component

--- src/CustomRoute.jsx
+++ src/CustomRoute.jsx
@@ -2,42 +2,47 @@ import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { auth } from "./firebase";
 import { useAuthStore } from "./context/store";
-import Navbar from "./components/Navbar"
-import Login from './components/Auth/Login'
-import Signup from './components/Auth/Signup'
-import Dashboard from "./components/Dashboard/Dashboard"
-import ProtectedRoute from "./components/ProtectedRoute"
-import ForgotPassword from './components/Auth/ForgotPassword'
+import Navbar from "./components/Navbar";
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
+import Dashboard from "./components/Dashboard/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ForgotPassword from "./components/Auth/ForgotPassword";
 
 function CustomRoute() {
   const { setUser, setLoading } = useAuthStore();
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(async (user) => {
-      if(user) {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
         setUser(user);
       } else {
-        setUser(null)
+        setUser(null);
       }
-      setLoading(false)
-    })
-    return unsubscribe()
-  }, [setUser, setLoading])
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, [setUser, setLoading]);
+
   return (
     <Router>
       <Navbar />
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/dashboard" element={<ProtectedRoute>
-        <Dashboard />
-      </ProtectedRoute>}
-      />
-    </Routes>
-  </Router>
-  )
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default CustomRoute;
